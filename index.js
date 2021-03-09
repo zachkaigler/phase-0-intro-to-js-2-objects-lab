3edce64c2b714ac0caa3ead2e71a516e68f00640
Zach Kaigler <zachkaiglerfilm@gmail.com>
2021-03-09 12:31:00 -0500

    Done with assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const employee = {
+    name: "Zach Kaigler",
+    streetAddress: "283 5th Ave"
+}
+
+function updateEmployeeWithKeyAndValue(object, key, value) {
+    return Object.assign({}, object, {[key]: value}); 
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(object, key, value) {
+    return Object.assign(object, {[key]: value});
+}
+
+function deleteFromEmployeeByKey(object, key) {
+    const newObject = Object.assign({}, object);
+    delete newObject[key];
+    return newObject;
+}
+
+function destructivelyDeleteFromEmployeeByKey(object, key) {
+    delete object[key];
+    return object;
+}
